8cafc9871697de7f5a30405ce0edcca04cca6ca1
Ty Potter <tyler.john.potter@gmail.com>
2017-10-31 00:21:01 -0600

    use 'module' and 'action' for the remote module calls

--- photopi/client/module.py
+++ photopi/client/module.py
@@ -9,13 +9,15 @@ class ClientModule(Borg):
         Borg.__init__(self)
 
     def main(self, args):
+        print("Client Module")
         print(args)
         # rpyc client
         conn = rpyc.connect(args['--host'], int(args['--port']))
         c = conn.root
 
-        if args['raspistill']:
+        if args['--module'] == "raspistill":
             module = c.raspistill()
+            args[args['action']] = True
             module.main(args)
 
 def get_module():
