8e2180fd8c4bfcd1c4a793573f9f972cdc4671d9
Ty Potter <tyler.john.potter@gmail.com>
2017-10-30 21:47:36 -0600

    raspistill test command

--- photopi/raspistill/spec.py
+++ photopi/raspistill/spec.py
@@ -1,19 +1,37 @@
 import os
+from subprocess import Popen, PIPE
+from threading import Thread
 
-class RaspistillSpec():
+class RaspistillSpec(Thread):
 
     def Test(path=None):
-        spec = RaspistillSpec(output = os.path.join(path, "test.jpg"))
+        spec = RaspistillSpec(output = os.path.join(path, "test.jpg"), quality=5)
         return spec
 
-    def __init__(self, label=None, output=None):
+    def __init__(self, label=None, output=None, quality=75):
+        Thread.__init__(self)
         self.label = label
         self.output = output
+        self.quality = quality
 
     def __str__(self):
         return str(self.__dict__)
 
-    def call(self):
-        print("raspistill cmd")
+    def _get_cmd(self):
+        args = []
+        if self.output:
+            args = args + ["-o", self.output]
+        if self.quality:
+            args = args + ["-q", str(self.quality)]
 
-        print("raspistill -o {}".format(self.output))
+        cmd = ["raspistill"] + args
+        return cmd
+
+    def run(self):
+        p = Popen(self._get_cmd(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+        self.stdout = p.stdout
+
+        self.output, self.err = p.communicate()
+        self.returncode = p.returncode
+
