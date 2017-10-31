db67ad0602b618b73713ef345452eaf7b2c45625
Ty Potter <tyler.john.potter@gmail.com>
2017-10-30 22:19:19 -0600

    beginnings of timelapse command

--- photopi/raspistill/spec.py
+++ photopi/raspistill/spec.py
@@ -4,15 +4,36 @@ from threading import Thread
 
 class RaspistillSpec(Thread):
 
-    def Test(path=None):
-        spec = RaspistillSpec(output = os.path.join(path, "test.jpg"), quality=5)
+    def Test(path=None, verbose=False):
+        spec = RaspistillSpec(path=path,
+            output="test.jpg",
+            verbose=verbose,
+            quality=5)
         return spec
 
-    def __init__(self, label=None, output=None, quality=75):
+    def Timelapse(path=None, label=None, interval=None, timeout=None, verbose=True):
+        if not interval:
+            interval = 5000
+        if not timeout:
+            timeout = 32400000
+
+        spec = RaspistillSpec(
+            path=os.path.join(path, label),
+            output="image%06d.jpg",
+            interval=interval,
+            timeout=timeout,
+            verbose=verbose)
+        return spec
+
+    def __init__(self, label=None, output=None, quality=75, path=None, verbose=False, timeout=None, interval=None):
         Thread.__init__(self)
+        self.path = path
         self.label = label
         self.output = output
         self.quality = quality
+        self.verbose = verbose
+        self.timeout = timeout
+        self.interval = interval
 
     def __str__(self):
         return str(self.__dict__)
@@ -20,14 +41,21 @@ class RaspistillSpec(Thread):
     def _get_cmd(self):
         args = []
         if self.output:
-            args = args + ["-o", self.output]
+            args = args + ["-o", os.path.join(self.path, self.output)]
         if self.quality:
             args = args + ["-q", str(self.quality)]
+        if self.verbose:
+            args = args + ["-v"]
+        if self.interval:
+            args = args + ["-tl", str(self.interval)]
+        if self.timeout:
+            args = args + ["-t", str(self.timeout)]
 
         cmd = ["raspistill"] + args
         return cmd
 
     def run(self):
+        print(self._get_cmd())
         p = Popen(self._get_cmd(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
         self.stdout = p.stdout
