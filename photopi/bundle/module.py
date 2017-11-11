041cd3f5524b97cbee1d3628225b43b75315b005
Ty Potter <tyler.john.potter@gmail.com>
2017-11-11 16:42:14 -0700

    allow bundle ls to be filtered by label, and make the filtering more efficient

--- photopi/bundle/module.py
+++ photopi/bundle/module.py
@@ -32,25 +32,28 @@ class BundleModule(Borg):
 
         bundles = {}
         for key, path in nodes:
-            bundles[key] = self._get_bundles(path)
+            bundles[key] = self._get_bundles(path, args['--device'], args['--label'])
 
         for key, bundle in bundles.items():
             print(key)
             for device, labels in bundle.items():
-                if not args['--device'] or device == args['--device']:
-                    print("\t{}".format(device))
-                    for label in labels:
-                        print("\t\t{}".format(label))
+                print("\t{}".format(device))
+                for label in labels:
+                    print("\t\t{}".format(label))
 
-    def _get_bundles(self, path):
+    def _get_bundles(self, path, device_lim=None, label_lim=None):
 
         archives = {}
         for root, devices, __ in os.walk(path):
             for device in devices:
+                if device_lim and device != device_lim:
+                    continue
                 archives[device] = []
                 for ___, ____, files in os.walk(os.path.join(root, device)):
                     for filename in fnmatch.filter(files, "*.tar.gz"):
                         label = BundleSort.get_label(filename)
+                        if label_lim and label != label_lim:
+                            continue
                         if label not in archives[device]:
                             archives[device].append(label)
 
