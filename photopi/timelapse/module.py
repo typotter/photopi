f033a6aaecefec8142d04b531406ace826b0e214
Ty Potter <tyler.john.potter@gmail.com>
2017-11-18 22:52:04 -0700

    put timelapse in a loop

--- photopi/timelapse/module.py
+++ photopi/timelapse/module.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import logging
 import os
+import time
 
 from photopi.core.borg import Borg
 from photopi.core.cmd import RsyncCmd
@@ -38,75 +39,81 @@ class TimelapseModule(Borg):
         return self._suite(args, config)
 
     def _suite(self, args, config):
-        self._log.debug(args)
-        self._log.debug(config)
+        done = False
+        while not done:
+            self._log.debug(args)
+            self._log.debug(config)
 
-        bundle_mod = BundleModule()
-        bundles = bundle_mod.filter_bundles(args, config)
+            bundle_mod = BundleModule()
+            bundles = bundle_mod.filter_bundles(args, config)
 
-        self._log.info(bundles)
+            self._log.info(bundles)
 
-        nodes = sorted(list(bundles.keys()))
-        if not nodes:
-            self._log.info("No nodes reachable. Exiting")
-            return True
-        elif len(nodes) > 1:
-            node = _prompt("Select Node> ", nodes)
-        else:
-            node = nodes[0]
+            nodes = sorted(list(bundles.keys()))
+            if not nodes:
+                self._log.info("No nodes reachable. Exiting")
+                return True
+            elif len(nodes) > 1:
+                node = _prompt("Select Node> ", nodes)
+            else:
+                node = nodes[0]
 
-        devices = sorted(bundles[node])
-        if not devices:
-            self._log.info("No devices. Exiting")
-            return True
-        elif len(devices) > 1:
-            device = _prompt("Select Device> ", devices)
-        else:
-            device = devices[0]
+            devices = sorted(bundles[node])
+            if not devices:
+                self._log.info("No devices. Exiting")
+                return True
+            elif len(devices) > 1:
+                device = _prompt("Select Device> ", devices)
+            else:
+                device = devices[0]
 
-        labels = sorted(bundles[node][device])
-        if not labels:
-            self._log.info("No bundles. Exiting")
-            return True
-        elif len(labels) > 1:
-            label = _prompt("Select Label> ", labels)
-        else:
-            label = labels[0]
+            labels = sorted(bundles[node][device])
+            if not labels:
+                self._log.info("No bundles. Exiting")
+                return True
+            elif len(labels) > 1:
+                label = _prompt("Select Label> ", labels)
+            else:
+                label = labels[0]
 
-        bundle = BundleSpec(device, label, config.storage_node(node))
+            bundle = BundleSpec(device, label, config.storage_node(node))
 
-        print("Processing {}/{}/{}".format(node, device, label))
+            print("Processing {}/{}/{}".format(node, device, label))
 
-        loaded = bundle_mod.expand(bundle, config)
+            loaded = bundle_mod.expand(bundle, config)
 
-        if not loaded:
-            self._log.error("Unable to load")
-            return False
+            if not loaded:
+                self._log.error("Unable to load")
+                return False
 
-        destnode = _prompt("Select destination node> ",
-                           list(config.storage_nodes.keys()))
+            destnode = _prompt("Select destination node> ",
+                               list(config.storage_nodes.keys()))
 
-        destpath = config.storage_node(destnode)
-        if not destpath:
-            self._log.warning("node [%s] is not configured",
-                              args['--node'])
-            return False
+            destpath = config.storage_node(destnode)
+            if not destpath:
+                self._log.warning("node [%s] is not configured",
+                                  args['--node'])
+                return False
 
-        fname = input("Enter timelapse name> ")
+            fname = input("Enter timelapse name> ")
 
-        dest_avi = os.path.join(destpath,
-                                "{}-{}-timelapse-{}.avi".format(
-                                    bundle.device, bundle.label, fname))
+            dest_avi = os.path.join(destpath,
+                                    "{}-{}-timelapse-{}.avi".format(
+                                        bundle.device, bundle.label, fname))
 
-        loadedpath = os.path.join(config.swap_path, bundle.device, bundle.label)
+            loadedpath = os.path.join(config.swap_path, bundle.device,
+                                      bundle.label)
 
-        cmd = MencoderCmd.AllFiles(loadedpath, dest_avi)
-        cmd.start()
+            cmd = MencoderCmd.AllFiles(loadedpath, dest_avi)
+            cmd.start()
 
-        while not cmd.is_alive():
-            time.sleep(1)
+            while not cmd.is_alive():
+                time.sleep(1)
 
-        return cmd.returncode == 0 or cmd.returncode == None
+            cont = input("timelapse complete. Another? Y/n")
+            done = cont == "y" or cont == "Y"
+
+        return True
 
 
 MODULE = ("timelapse", TimelapseModule)
